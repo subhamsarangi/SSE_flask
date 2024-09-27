1854278ec29def66d28df31e37187d7ff87dff7c
Subham Sarangi <subhamsarangi123@gmail.com>
2024-09-27 22:06:17 +0530

    Update app_temp.js

--- static/js/app_temp.js
+++ static/js/app_temp.js
@@ -2,5 +2,5 @@ const eventSource = new EventSource('/stream_temp');
 
 eventSource.onmessage = function(event) {
     const data = JSON.parse(event.data);
-    $('#temp').text(data.text);
+    $('#text').text(data.text);
 };
